50dc918aed794c36a5d0bd84fe80701a342993f8
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2023-09-02 10:24:49 -0700

    Added model

--- ComputeCanadaFiles/datadown.py
+++ ComputeCanadaFiles/datadown.py
@@ -1,6 +1,4 @@
-import mne
-subject = [1,2,3]
+from Data import download_EEGBCI 
 total_subjects = list(range(1, 110))
 runs = list(range(1, 15))
-for sub in total_subjects:
-    download = mne.datasets.eegbci.load_data(sub, runs, './', verbose=False)
+download_EEGBCI(total_subjects, runs, './EEGData', False)
